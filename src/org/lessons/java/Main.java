aedddc2f10b8367a85b6747d2c6b63e346521bc1
Pastafredda <justspotify09@gmail.com>
2023-10-26 15:32:43 +0200

    fine primo

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,2 +1,35 @@
-package org.lessons.java;public class Main {
+package org.lessons.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //chiedere i dati del regalo ed inserirli in un array list
+         Scanner scanner = new Scanner(System.in);
+
+        //creazione array list
+        List<Regalo> regali = new ArrayList<>();
+
+        //creazione variabile per fermarsi
+        boolean exit= false;
+
+        while (!exit){
+            System.out.println("Vuoi inserire un nuovo regalo? si/no");
+            exit= scanner.nextLine().equals("no");
+            if (!exit){
+                System.out.println("Inserisci la descrizione del regalo");
+                String descrizione= scanner.nextLine();
+                System.out.println("Inserisci il destinatario");
+                String destinatario=scanner.nextLine();
+                regali.add(new Regalo(descrizione, destinatario));
+                System.out.println(regali.size());
+            }
+        }
+        System.out.println(regali.size());
+        System.out.println(regali.toString());
+        scanner.close();
+    }
+
 }
--- src/org/lessons/java/Regalo.java
+++ src/org/lessons/java/Regalo.java
@@ -1,2 +1,27 @@
-package org.lessons.java;public class Regalo {
+package org.lessons.java;
+
+public class Regalo {
+
+    private String destinatario;
+    private String descrizione;
+
+    public Regalo(String descrizione, String destinatario) {
+        this.descrizione = descrizione;
+        this.destinatario= destinatario;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "descrizione'" + descrizione + '\'' +
+                ", destinatario" + destinatario +
+                '}';
+    }
 }
